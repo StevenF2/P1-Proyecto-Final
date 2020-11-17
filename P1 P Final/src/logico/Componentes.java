9a80145e4b33036b212bc0c53a5b6f537a10f4df
Nasu4life <Nasu4life@Nasu4life>
2020-11-17 16:09:35 -0400

    Primera clase (y padre de las otras que mostramos en el UML)

---
+++ P1 P Final/src/logico/Componentes.java
@@ -0,0 +1,36 @@
+package logico;
+
+public abstract class Componentes {
+	
+	float precio;
+	int cantDisponible;
+	int numSerie;
+	
+	public Componentes(float precio, int cantDisponible, int numSerie) {
+		super();
+		this.precio = precio;
+		this.cantDisponible = cantDisponible;
+		this.numSerie = numSerie;
+	}
+	
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getCantDisponible() {
+		return cantDisponible;
+	}
+	public void setCantDisponible(int cantDisponible) {
+		this.cantDisponible = cantDisponible;
+	}
+	public int getNumSerie() {
+		return numSerie;
+	}
+	public void setNumSerie(int numSerie) {
+		this.numSerie = numSerie;
+	}
+
+	
+}
