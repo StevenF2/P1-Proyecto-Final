786ad2bc3b6d289ce6fe9799fe667062c60c9154
LissBn <lissabn9@gmail.com>
2020-11-23 22:09:51 -0400

    Combos en el menu ventas y Ordenes de compra en el menu proveedores

--- P1 P Final/src/visual/InterfazPrincipal.java
+++ P1 P Final/src/visual/InterfazPrincipal.java
@@ -42,15 +42,6 @@ public class InterfazPrincipal extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenu mnNewMenu_5 = new JMenu("Combos");
-		menuBar.add(mnNewMenu_5);
-		
-		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Nuevo Combo");
-		mnNewMenu_5.add(mntmNewMenuItem_8);
-		
-		JMenuItem mntmNewMenuItem_9 = new JMenuItem("Ver Combos Disponibles");
-		mnNewMenu_5.add(mntmNewMenuItem_9);
-		
 		JMenu mnNewMenu = new JMenu("Empleados");
 		menuBar.add(mnNewMenu);
 		
@@ -66,14 +57,23 @@ public class InterfazPrincipal extends JFrame {
 		JMenuItem mntmNewMenuItem = new JMenuItem("Ver Inventario");
 		mnNewMenu_2.add(mntmNewMenuItem);
 		
-		JMenu mnNewMenu_4 = new JMenu("Ordenes de Compra");
-		menuBar.add(mnNewMenu_4);
+		JMenu mnNewMenu_5 = new JMenu("Combos");
+		mnNewMenu_2.add(mnNewMenu_5);
 		
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Historial de Ordenes");
-		mnNewMenu_4.add(mntmNewMenuItem_4);
+		JMenuItem mntmNewMenuItem_8 = new JMenuItem("Nuevo Combo");
+		mnNewMenu_5.add(mntmNewMenuItem_8);
 		
-		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Realizar Orden de Compra");
-		mnNewMenu_4.add(mntmNewMenuItem_5);
+		JMenuItem mntmNewMenuItem_9 = new JMenuItem("Ver Combos Disponibles");
+		mnNewMenu_5.add(mntmNewMenuItem_9);
+		
+		JMenu mnNewMenu_1 = new JMenu("Ventas");
+		menuBar.add(mnNewMenu_1);
+		
+		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Historial de Ventas");
+		mnNewMenu_1.add(mntmNewMenuItem_7);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Realizar Venta");
+		mnNewMenu_1.add(mntmNewMenuItem_2);
 		
 		JMenu mnNewMenu_3 = new JMenu("Proveedores");
 		menuBar.add(mnNewMenu_3);
@@ -84,14 +84,14 @@ public class InterfazPrincipal extends JFrame {
 		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Ver Proveedores Activos");
 		mnNewMenu_3.add(mntmNewMenuItem_3);
 		
-		JMenu mnNewMenu_1 = new JMenu("Ventas");
-		menuBar.add(mnNewMenu_1);
+		JMenu mnNewMenu_4 = new JMenu("Ordenes de Compra");
+		mnNewMenu_3.add(mnNewMenu_4);
 		
-		JMenuItem mntmNewMenuItem_7 = new JMenuItem("Historial de Ventas");
-		mnNewMenu_1.add(mntmNewMenuItem_7);
+		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Historial de Ordenes");
+		mnNewMenu_4.add(mntmNewMenuItem_4);
 		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Realizar Venta");
-		mnNewMenu_1.add(mntmNewMenuItem_2);
+		JMenuItem mntmNewMenuItem_5 = new JMenuItem("Realizar Orden de Compra");
+		mnNewMenu_4.add(mntmNewMenuItem_5);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
